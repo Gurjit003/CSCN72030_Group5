2f5206d3fd7ba2e714df6577ff19506ed4bf227c
Kenneth Targosz <Ktargosz8785@conestogac.on.ca>
2022-12-01 21:12:38 -0500

    added the ability to clean a single table by clicking on it

--- GUI_Module/FrontOfHouse .cs
+++ GUI_Module/FrontOfHouse .cs
@@ -53,5 +53,101 @@ namespace GUI_Module
             }
 
         }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            frontOfHouse.arrayOfTables[0].clearTable();
+            updateTableStatus();
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            frontOfHouse.arrayOfTables[1].clearTable();
+            updateTableStatus();
+        }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+            frontOfHouse.arrayOfTables[2].clearTable();
+            updateTableStatus();
+        }
+
+        private void pictureBox7_Click(object sender, EventArgs e)
+        {
+            frontOfHouse.arrayOfTables[3].clearTable();
+            updateTableStatus();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            frontOfHouse.arrayOfTables[4].clearTable();
+            updateTableStatus();
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            frontOfHouse.arrayOfTables[5].clearTable();
+            updateTableStatus();
+        }
+
+        private void pictureBox6_Click(object sender, EventArgs e)
+        {
+            frontOfHouse.arrayOfTables[6].clearTable();
+            updateTableStatus();
+        }
+
+        private void pictureBox8_Click(object sender, EventArgs e)
+        {
+            frontOfHouse.arrayOfTables[7].clearTable();
+            updateTableStatus();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            frontOfHouse.arrayOfTables[0].clearTable();
+            updateTableStatus();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            frontOfHouse.arrayOfTables[1].clearTable();
+            updateTableStatus();
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+            frontOfHouse.arrayOfTables[2].clearTable();
+            updateTableStatus();
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+            frontOfHouse.arrayOfTables[3].clearTable();
+            updateTableStatus();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            frontOfHouse.arrayOfTables[4].clearTable();
+            updateTableStatus();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            frontOfHouse.arrayOfTables[5].clearTable();
+            updateTableStatus();
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+            frontOfHouse.arrayOfTables[6].clearTable();
+            updateTableStatus();
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+            frontOfHouse.arrayOfTables[7].clearTable();
+            updateTableStatus();
+        }
     }
 }
